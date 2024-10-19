d61be5b64aa5f557b26d8a6ca3b47715f7d9b2c0
oxxultus <46725938+oxxultus@users.noreply.github.com>
2024-10-19 19:13:01 +0900

    Delete java-maven directory

--- java-maven/src/main/java/cse/oop2/ch02/game/GameLauncher.java
+++
@@ -1,8 +0,0 @@
-package cse.oop2.ch02.game;
-
-public class GameLauncher {
-    public static void main(String[] args) {
-        GuessGame game = new GuessGame();
-        game.startGame();
-    }
-}
